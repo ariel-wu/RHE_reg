70f0688f260617fb55a970d81018df08c086de42
yue Wu <arielwu@veena.cs.ucla.edu>
2018-03-14 16:41:42 -0700

    normalize-fixed

--- helper/plink_simulator.cpp
+++ helper/plink_simulator.cpp
@@ -109,21 +109,20 @@ int main(int argc, char const *argv[])
 	}
 	fclose(fmap); 
 	fprintf(fppheno, "%s %s %d \n", "FID", "IID", 1); 
+	ublas::matrix<int> G(n, m); 
 	for(int i=0; i<n; i++){
 	//simulate each individual
 		fprintf(fped, "%d %d %d %d %d %d", i,1,0,0,0,0); 
 		ublas::matrix<int> G1(1,m); 
 		ublas::matrix<int> G2(1,m); 
-		ublas::matrix<int> G(1,m);
-		ublas::matrix<double> X(1,m);
-		accumulator_set<double, stats<tag::mean, tag::moment<2> > > acc; 
+		//accumulator_set<double, stats<tag::mean, tag::moment<2> > > acc; 
 		for(int j=0; j<m; j++)
 		{
 		double temp1=geno(); double temp2=geno(); 
 			G1(0,j) = (temp1<=panc[j]); G2(0,j)=(temp2<=panc[j]); 
-//			fprintf(fpgeno, "%d", G1(0,j)+G2(0,j)); 
 			int val= G1(0,j)+ G2(0,j);
-			acc(val); 
+		//	acc(val); 
+			G(i,j) = val; 
 			bool temp=(j < m/2); 
 			if(val==0)
 			{
@@ -146,20 +145,27 @@ int main(int argc, char const *argv[])
 
 		}
 		fprintf(fped, "\n"); 
-		G = G1+G2;
-	//	fprintf(fpgeno, "\n");  
-		double g_mean = mean(acc);
-		double q = 0.5 *g_mean; 
-		double g_var =sqrt(2* q *(1-q)) ; 
-		for(int j=0;j<m; j++)
-			X(0,j) = (G(0,j)-g_mean) / g_var;
-		//cout<< "g mean : "<<g_mean<<endl<<"g std: "<<g_var<<endl ; 
-		//cout<<G<<endl;
-		//cout<<X <<endl;
-		ublas::matrix<double> pheno = prod(X,beta);  
-		fprintf(fppheno, "%d %d %f", i, 1,pheno(0,0)+env(i,0)); 
+	}
+	ublas::matrix<double> a(1,n, 1); 
+	ublas::matrix<double> g_mean = prod(a, G);
+	g_mean = g_mean / n; 
+	ublas::matrix<double> g_var(1, m); 
+	for(int i=0; i<m; i++){
+		double q = 0.5 * g_mean(0,i); 
+		g_var(0,i) = sqrt(2*q*(1-q)); 
+		if(g_var(0,i)==0)
+			g_var(0,i)=1; 
+	}
+	for(int i=0; i<n; i++)
+	{
+		ublas::matrix<double> X(1, m); 
+		for(int j=0; j<m; j++)
+			X(0, j) = (G(i,j)- g_mean(0,j)) / g_var(0,j) ; 
+		ublas::matrix<double> pheno = prod(X, beta); 
+		fprintf(fppheno, "%d %d %f", i, 1,pheno(0,0)+env(i,0));
 		fprintf(fppheno, "\n"); 
 	}
+	 
 	fclose(fped); 
 	fclose(fppheno); 
 }
